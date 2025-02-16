38e311d83aeadab9fded784aaa3191cde9bded09
Juan Bernal <mesieou@gmail.com>
2025-02-16 12:58:23 +1100

    initial styling of the contact seciton

--- src/app/components/contactsection.tsx
+++ src/app/components/contactsection.tsx
@@ -1,15 +1,7 @@
 
 export default function ContactSection() {
   return (
-    <section className="section3" id="contact">
-      <div className="form-title-description">
-        <h1>Contact us</h1>
-        <h2 className="no-space-h3">Get in touch with us</h2>
-        <p>Phone: 0473 164 581</p>
-        <p>Email: info@thebestmove.com.au </p>
-        <br />
-        <p>Call us or send us a message to book your move today</p>
-      </div>
+    <section className="flex min-h-screen items-center gap-5" id="contact">
       <div className="contact-form ">
         <form>
           <input type="text" placeholder="Name" />
@@ -18,6 +10,14 @@ export default function ContactSection() {
           <textarea placeholder="Message"></textarea>
           <button>Send</button>
         </form>
+      </div>
+      <div className="form-title-description">
+        <h1>Contact us</h1>
+        <h2 className="no-space-h3">Get in touch with us</h2>
+        <p>Phone: 0473 164 581</p>
+        <p>Email: info@thebestmove.com.au </p>
+        <br />
+        <p>Call us or send us a message to book your move today</p>
       </div>
     </section>
   );
